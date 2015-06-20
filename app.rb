b47cf74f27187fa474a8876def42fc00486bc108
MaxMEllon <mozi_kke@excite.co.jp>
2015-06-21 03:35:41 +0900

    :arrow_up_small: update

--- app.rb
+++ app.rb
@@ -3,6 +3,26 @@
 require 'bundler'
 Bundler.require
 
+conn = PGconn.connect(
+  host: 'localhost',
+  user: 'maxmellon',
+  dbname: 'maxmellon'
+)
+
+set :public_folder, File.dirname(__FILE__) + '/assets'
+
+helpers do
+  def link_to(url, txt = url)
+    "<a href=\"#{url}\">#{txt}</a>"
+  end
+end
+
 get '/?' do
   slim :index
+end
+
+get '/show/?' do
+  query = 'SELECT * FROM stock'
+  @res = conn.exec(query)
+  slim :show
 end
